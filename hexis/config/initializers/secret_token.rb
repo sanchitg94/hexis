e2f9c370700c29360f4e72c143bc753c9cb63e66
lazerhacker <lazerhacker@rlazerowitz-mbp15-1.local>
2013-07-12 21:32:38 -0700

    Set it up

---
+++ hexis/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hexis::Application.config.secret_key_base = '671f042c1dc311d8232b80f3d1260c4350b0f1c03c9a9bbfde148bd39ca1fb67778ded499f99e877dcbfee783016146aada39783d7a0a65149cb46f75fa62d27'
